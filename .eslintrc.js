223a7a248f79edbde905722d5e37788b524a8874
oldbelom <oldbelom@gmail.com>
2022-03-05 11:06:33 +0300

    setup project

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react"],
+  extends: [
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ["**/dist/**"],
+  rules: {
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+        code: 100,
+      },
+    ],
+    "import/prefer-default-export": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
